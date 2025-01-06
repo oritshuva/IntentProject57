78885d36af4e376766b805c34293639d5dc67e19
oritshuva <ori.tshuva87@gmail.com>
2025-01-06 16:47:54 +0200

    bug fixes

--- app/src/main/java/com/example/intentapp/ShareActivity.java
+++ app/src/main/java/com/example/intentapp/ShareActivity.java
@@ -5,11 +5,7 @@ import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class ShareActivity extends AppCompatActivity {
 
